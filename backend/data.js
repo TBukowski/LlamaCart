5b0cece601ddad2a224ef67affb60978aad59a88
Ty Bukowski <shuteman2@aol.com>
2021-07-06 21:15:57 -0500

    added .gitignore, fixed minor spelling errors in data.js, server.js to help run npm commands

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,4 @@
-module.export = {
+module.exports = {
     products: [
         {
             _id: '1',
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const data = require('./data.js');
 const app = express();
 
-app.get("/api/products", (req, res) => {
+app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
 
